1eed9d7f64bef2050caca04380b73cdc901a4311
Brooklyn-0 <75869809+Brooklyn-0@users.noreply.github.com>
2021-05-16 00:20:15 +0800

    Create Ifetc32.v

---
+++ component/lys version/Ifetc32.v
@@ -0,0 +1,24 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2021/05/15 22:48:25
+// Design Name: 
+// Module Name: Ifetc32
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Ifetc32(
+    input[31:0]  A
